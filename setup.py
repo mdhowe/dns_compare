ba45c45264a83164ebb5b9d5e45353ae4c69efdc
Dave van Duivenbode <dave.v.duivenbode@gmail.com>
2020-11-19 08:24:31 +0100

    Bumped version in setup.py to 0.0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='dns_compare',
-    version='0.0.3',
-    author='Joe Miller',
-    author_email='joeym@joeym.net',
+    version='0.0.4',
+    author='Joe Miller, Dave van Duivenbode',
+    author_email='joeym@joeym.net, dave.v.duivenbode@gmail.com',
     scripts=['dns_compare'],
-    url='https://github.com/joemiller/dns_compare',
+    url='https://github.com/Sefiris/dns_compare',
     license='LICENSE',
-    description="Test DNS servers",
-    long_description=open('README.markdown').read()
+    description="Compare data from a BIND zone file to data returned from a DNS server",
+    long_description=open('README.md').read()
 )
